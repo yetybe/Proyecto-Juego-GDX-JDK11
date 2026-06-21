50be9edc6f8482a159f256bda542c8f0dcc2d807
vicen <vicen@DESKTOP-P3TQL7U>
2026-06-21 00:48:41 -0400

    game over finished wallahi

--- Proyecto-Juego-GDX-core/src/main/java/io/github/Pantallas/PantallaGameOver.java
+++ Proyecto-Juego-GDX-core/src/main/java/io/github/Pantallas/PantallaGameOver.java
@@ -3,79 +3,92 @@ package io.github.Pantallas;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.utils.ScreenUtils;
 import io.github.Main.SpaceNavigation;
-
 
 public class PantallaGameOver implements Screen {
 
 	private SpaceNavigation game;
 	private OrthographicCamera camera;
+	private Texture txGameOver;
+	private GlyphLayout layout;
 
 	public PantallaGameOver(SpaceNavigation game) {
 		this.game = game;
         
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 1200, 800);
+
+		this.txGameOver = new Texture(Gdx.files.internal("game_over.png"));
+		this.layout = new GlyphLayout();
 	}
 
 	@Override
 	public void render(float delta) {
 		ScreenUtils.clear(0, 0, 0.2f, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		camera.update();
 		game.getBatch().setProjectionMatrix(camera.combined);
 
 		game.getBatch().begin();
-		game.getFont().draw(game.getBatch(), "Game Over !!! ", 120, 400,400,1,true);
-		game.getFont().draw(game.getBatch(), "Pincha en cualquier lado para reiniciar ...", 100, 300);
+
+		float anchoPantalla = camera.viewportWidth;
+		float altoPantalla = camera.viewportHeight;
+
+		float anchoImg = txGameOver.getWidth();
+		float altoImg = txGameOver.getHeight();
+		float xImg = (anchoPantalla - anchoImg) / 2;
+		float yImg = (altoPantalla - altoImg) / 2 + 50;
+
+		game.getBatch().draw(txGameOver, xImg, yImg, anchoImg, altoImg);
+
+		String textoInstruccion = "Pincha en cualquier lado para reiniciar ...";
+		layout.setText(game.getFont(), textoInstruccion);
+		float xTexto = (anchoPantalla - layout.width) / 2;
+		float yTexto = yImg - 50;
+
+		game.getFont().draw(game.getBatch(), textoInstruccion, xTexto, yTexto);
 	
 		game.getBatch().end();
 
 		if (Gdx.input.isTouched() || Gdx.input.isKeyJustPressed(Input.Keys.ANY_KEY)) {
-			Screen ss = new PantallaJuego(game,1,10, false);
+			Screen ss = new PantallaJuego(game, 1, 10, false);
 			ss.resize(1200, 800);
 			game.setScreen(ss);
 			dispose();
 		}
 	}
  
-	
 	@Override
 	public void show() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void pause() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void resume() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void hide() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
-		
+		if (txGameOver != null) {
+			txGameOver.dispose();
+		}
 	}
-   
 }
